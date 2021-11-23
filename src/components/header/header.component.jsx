982b90b201b6b292472668d0b2b3ada60e7806ec
Yaya <bedourayannis@hotmail.com>
2021-11-23 21:10:28 +0100

    finishing sign in with handleSubmit method and removing link in sign out from the header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -16,7 +16,7 @@ const Header = ({currentUser}) => (
             <Link className="option" to="/contact">contact</Link>
             {
                 currentUser ?
-                (<div className="option" onClick={()=> auth.signOut()}>Sign out</div>)
+                (<Link className="option" onClick={()=> auth.signOut()}>Sign out</Link>)
                 :
                 (<Link className="option" to="/signin">Sign In</Link>)
             }
